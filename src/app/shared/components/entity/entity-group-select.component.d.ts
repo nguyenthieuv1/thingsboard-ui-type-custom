8f5194fb98ff4fc8f255e176f997d2288abd3c12
ThieuNN <ThieuNN@bkav.com>
2025-07-29 14:58:10 +0700

    them input cho EntityGroupSelectComponent

--- src/app/shared/components/entity/entity-group-select.component.d.ts
+++ src/app/shared/components/entity/entity-group-select.component.d.ts
@@ -2,27 +2,58 @@ import { OnInit } from '@angular/core';
 import { EntityType } from "@shared/models/entity-type.models";
 import { BaseData, HasId } from "@shared/models/base-data";
 import { ControlValueAccessor, UntypedFormBuilder, UntypedFormGroup } from "@angular/forms";
+import { AutocompleteEntityGroupComponent } from "@shared/components/bkav/autocomplete-entity-group.component";
+import * as i0 from "@angular/core";
+
 export declare class EntityGroupSelectComponent implements ControlValueAccessor, OnInit {
     fb: UntypedFormBuilder;
-    protected readonly EntityType: any;
+    protected readonly EntityType: typeof EntityType;
     entityGroupSelectForm: UntypedFormGroup;
     owner: BaseData<HasId>;
-    childGroupAutocomplete: any;
+    childGroupAutocomplete: AutocompleteEntityGroupComponent;
     protected propagateChange: (v: any) => void;
+    
+    /** Có bao gồm group owner hay không */
     isContainGroupOwner: boolean;
+
+    /** Có bao gồm entity group "All" hay không */
     isContainEntityGroupAll: boolean;
+
+    /** Giá trị hiện tại của form */
     modelValue: {
-        entityType: any;
-        owner: any;
-        entityGroup: any;
+        entityType: EntityType | null;
+        owner: string | null;
+        entityGroup: string | null;
     };
+
     constructor(fb: UntypedFormBuilder);
+
     ngOnInit(): void;
+
     ownerChange(owner: BaseData<HasId>): void;
-    isEntityTypeSelected(): any;
+
+    isEntityTypeSelected(): boolean;
+
     registerOnChange(fn: any): void;
+
     registerOnTouched(fn: any): void;
+
     writeValue(obj: any): void;
-    updateView(entityType: EntityType | null, owner: string, entityGroup: string | null): void;
+
+    updateView(entityType: EntityType | null, owner: string | null, entityGroup: string | null): void;
+
     getAllowedEntityType(): Array<EntityType> | null;
+
+    static ɵfac: i0.ɵɵFactoryDeclaration<EntityGroupSelectComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<EntityGroupSelectComponent,
+        "tb-entity-group-select",
+        never,
+        {
+            "isContainGroupOwner": "isContainGroupOwner";
+            "isContainEntityGroupAll": "isContainEntityGroupAll";
+        },
+        {},
+        never,
+        never
+    >;
 }
